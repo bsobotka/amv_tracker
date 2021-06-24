dd46c56b2ed0310598c8a780bb3fbe2c3bc7f994
bsobotka <b.d.sobotka@gmail.com>
2021-06-24 12:48:01 -0500

    Tag window now makes use of any mutually exclusive tags the user has specified by enabling/disabling checkboxes accordingly.

--- misc_files/tag_checkboxes.py
+++ misc_files/tag_checkboxes.py
@@ -13,16 +13,27 @@ class TagWindow(QtWidgets.QDialog):
 		self.out_str = ''
 		self.tag_table_friendly = tag_table
 		self.tag_table_internal = common_vars.tag_table_lookup()[self.tag_table_friendly]
+		self.checked_tags = checked_tags
 		self.signalMapper = QtCore.QSignalMapper()
 		self.conn = sqlite3.connect(common_vars.tag_db())
+		self.checkGroup = QtWidgets.QButtonGroup()
+
+		self.vLayoutMaster = QtWidgets.QVBoxLayout()
+		self.vLayout = QtWidgets.QVBoxLayout()
+		hLayout = QtWidgets.QHBoxLayout()
+		scrollWidget = QtWidgets.QWidget()
+		scrollArea = QtWidgets.QScrollArea()
 
 		self.listOfTagData = [[tag[0], tag[1], tag[2]] for tag in self.conn.execute('SELECT * FROM {}'
 		                                                                    .format(self.tag_table_internal))]
 		self.listOfTagData.sort(key=lambda x: x[2])
 		self.listChecks = [QtWidgets.QCheckBox(tag[0]) for tag in self.listOfTagData]
+		for check in self.listChecks:
+			self.checkGroup.addButton(check)
+		self.checkGroup.setExclusive(False)
 
-		if checked_tags is not None:
-			self.checked_tag_names_list = checked_tags.split(';')
+		if self.checked_tags is not None:
+			self.checked_tag_names_list = self.checked_tags.split('; ')
 			self.checked_tag_num_list = []
 			for tn in self.checked_tag_names_list:
 				for ind in range(0, len(self.listOfTagData)):
@@ -32,12 +43,6 @@ class TagWindow(QtWidgets.QDialog):
 			self.checked_tag_num_list = []
 
 		self.check_lst = [] + self.checked_tag_num_list
-
-		vLayoutMaster = QtWidgets.QVBoxLayout()
-		vLayout = QtWidgets.QVBoxLayout()
-		hLayout = QtWidgets.QHBoxLayout()
-		scrollWidget = QtWidgets.QWidget()
-		scrollArea = QtWidgets.QScrollArea()
 
 		# Header
 		self.headerFont = QtGui.QFont()
@@ -52,7 +57,7 @@ class TagWindow(QtWidgets.QDialog):
 		# Checkboxes
 		ind = 0
 		for check in self.listChecks:
-			vLayout.addWidget(check)
+			self.vLayout.addWidget(check)
 
 			if check.text().lower() in self.checked_tag_names_list:
 				check.setChecked(True)
@@ -61,6 +66,9 @@ class TagWindow(QtWidgets.QDialog):
 			self.signalMapper.setMapping(check, ind)
 			ind += 1
 			check.clicked.connect(self.signalMapper.map)
+
+		if self.check_lst:
+			self.tags_exist()
 
 		# Back/Submit buttons
 		self.backButton = QtWidgets.QPushButton('Back')
@@ -72,24 +80,74 @@ class TagWindow(QtWidgets.QDialog):
 		hLayout.addWidget(self.backButton)
 		hLayout.addWidget(self.submitButton)
 
-		vLayoutMaster.addWidget(self.tagLabel, alignment=QtCore.Qt.AlignCenter)
-		vLayoutMaster.addSpacing(10)
-		scrollWidget.setLayout(vLayout)
+		self.vLayoutMaster.addWidget(self.tagLabel, alignment=QtCore.Qt.AlignCenter)
+		self.vLayoutMaster.addSpacing(10)
+		scrollWidget.setLayout(self.vLayout)
 		scrollArea.setWidget(scrollWidget)
-		vLayoutMaster.addWidget(scrollArea)
-		vLayoutMaster.addLayout(hLayout)
+		self.vLayoutMaster.addWidget(scrollArea)
+		self.vLayoutMaster.addLayout(hLayout)
 
 		# Signals/slots
 		self.backButton.clicked.connect(self.reject)
 		self.submitButton.clicked.connect(self.accept)
 		self.signalMapper.mapped.connect(self.update_tag_list)
+		self.signalMapper.mapped.connect(self.mut_excl_tags)
 
 		## Widget ##
-		self.setLayout(vLayoutMaster)
+		self.setLayout(self.vLayoutMaster)
 		self.setWindowTitle('Tags - ' + tag_table)
 		self.setFixedSize(240, 520)
 
+	def tags_exist(self):
+		# Method for disabling tag checkboxes if user clicks into tag window with some checkboxes already checked which
+		# themselves have mutually exclusive tags associated with them
+
+		tags_exist_conn = sqlite3.connect(common_vars.video_db())
+		tags_exist_cursor = tags_exist_conn.cursor()
+
+		for tag in self.checked_tags:
+			tags_exist_cursor.execute('SELECT tag_name, disable_tags FROM {}'.format(self.tag_table_internal))
+		disable_dict = {x[0]: x[1].split('; ') for x in tags_exist_cursor.fetchall() if x[1] is not None}
+		all_tags_to_dis = list(set([item for sublist in [v for k, v in disable_dict.items() if k.lower() in self.checked_tags]
+		                            for item in sublist]))
+		if '' in all_tags_to_dis:
+			all_tags_to_dis.remove('')
+
+		for cbox_ind in range(0, self.vLayout.count()):
+			cbox_wid = self.vLayout.itemAt(cbox_ind).widget()
+			if cbox_wid.text() in all_tags_to_dis:
+				cbox_wid.setDisabled(True)
+				cbox_wid.setChecked(False)
+
+	def mut_excl_tags(self, check_ind):
+		# Method for enabling/disabling mutually exclusive tag checkboxes based on which tags are checked
+
+		mut_excl_tag_conn = sqlite3.connect(common_vars.video_db())
+		mut_excl_tag_cursor = mut_excl_tag_conn.cursor()
+		checked_btn_wid = self.vLayout.itemAt(check_ind).widget()
+
+		mut_excl_tag_cursor.execute('SELECT disable_tags FROM {} WHERE tag_name = ?'.format(self.tag_table_internal),
+		                            (checked_btn_wid.text(),))
+		tags_to_disable = mut_excl_tag_cursor.fetchone()[0]
+		if tags_to_disable:
+			tags_to_disable_split = tags_to_disable.split('; ')
+		else:
+			tags_to_disable_split = []
+
+		for cbox_ind in range(0, self.vLayout.count()):
+			cbox_wid = self.vLayout.itemAt(cbox_ind).widget()
+			if cbox_wid.text() in tags_to_disable_split:
+				if checked_btn_wid.isChecked():
+					cbox_wid.setDisabled(True)
+					cbox_wid.setChecked(False)
+				else:
+					cbox_wid.setEnabled(True)
+
+		mut_excl_tag_conn.close()
+
 	def update_tag_list(self, check_num):
+		# Method for providing ;-delimited string of labels from any checked boxes
+
 		if check_num not in self.check_lst:
 			self.check_lst.append(check_num)
 		else:
@@ -101,4 +159,4 @@ class TagWindow(QtWidgets.QDialog):
 		self.out_str = ''
 
 		for tag in self.out_lst:
-			self.out_str += (tag.lower() + '; ')
+			self.out_str += (tag.lower() + '; ')		
